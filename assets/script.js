6a5c38a12d255f8b750065598aa0359a4225aec9
Matheus <131068785+Matheus-m-prado@users.noreply.github.com>
2023-05-22 21:37:59 -0300

    recolocando menu antigo

--- assets/script.js
+++ assets/script.js
@@ -1,3 +1,17 @@
+function menuShow() {
+    let menuMobile = document.querySelector('.mobile-menu');
+    if (menuMobile.classList.contains('open')) {
+        menuMobile.classList.remove('open');
+        document.querySelector('.icon').src = "assets/img/menu_white_36dp.svg";
+    } else {
+        menuMobile.classList.add('open');
+        document.querySelector('.icon').src = "assets/img/close_white_36dp.svg";
+    }
+}
+
+
+
+// Parallax
 let text = document.getElementById('text');
 let leaf = document.getElementById('leaf');
 let hill1 = document.getElementById('hill1');
@@ -14,3 +28,7 @@ window.addEventListener('scroll', () => {
     hill4.style.left = value * -1.5 + 'px';
     hill1.style.top = value * 1.5 + 'px';
 });
+
+
+
+
